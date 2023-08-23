81ec43a176871fe484b4c2cbef4e61dcb41a6c46
Ekaterina <110250031+SyrbuEN@users.noreply.github.com>
2023-08-23 18:21:30 +0300

    Spring Security

---
+++ library-project/src/main/java/ru/itgirl/libraryproject/service/UsersServiceImpl.java
@@ -0,0 +1,34 @@
+package ru.itgirl.libraryproject.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.itgirl.libraryproject.dto.UsersDto;
+import ru.itgirl.libraryproject.model.Users;
+import ru.itgirl.libraryproject.repository.UsersRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UsersServiceImpl implements UsersService{
+
+    private final UsersRepository usersRepository;
+
+    @Override
+    public UsersDto getUserRoles(String name, String password) {
+        List<Users> users = usersRepository.findUserByNameAndPassword(name, password);
+
+        List<String> roles = new ArrayList<>();
+        for (Users user: users) {
+            roles.add(user.getUser_role());
+        }
+
+        UsersDto newUser = new UsersDto();
+        newUser.setUser_name(users.get(0).getUser_name());
+        newUser.setUser_password(users.get(0).getUser_password());
+        newUser.setUser_roles(roles);
+
+        return newUser;
+    }
+}
